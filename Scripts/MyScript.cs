51ec66bfa5c385ef21f215c17c6ce5fee0ef9e04
Сергей Торговцев <torgovtsevsd@masterscada.onmicrosoft.com>
2025-01-21 00:22:55 +0300

    fix

--- Scripts/MyScript.cs
+++ Scripts/MyScript.cs
@@ -5,7 +5,7 @@ public class MyScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Start 2");
+        Debug.Log("Start 3");
     }
 
     // Update is called once per frame
